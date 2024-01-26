1ff8927bfe853930226c84834700a3b049cf4ffc
lnsvn <lsullivan301@yahoo.com>
2024-01-26 12:50:15 -0500

    starts Home page function, installs react router package, adds router and routes to App function (includes new Home function)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Header/>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div>
+            <body>
+                hi
+            </body>
+        </div>
+    )
+}
+
+export default Home
