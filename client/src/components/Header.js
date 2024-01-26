da2102d03be75dea31e62f0cea957452c155fd25
lnsvn <lsullivan301@yahoo.com>
2024-01-26 13:23:07 -0500

    creates constant navData file, and creates function to map navData in Header to display NavLinks

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import navLinks from "../constants/nav";
 
 function Header() {
+    const navData = navLinks.map((nav) => {
+        return (
+            <li>
+                <NavLink to={nav.path}>{nav.title}</NavLink>
+            </li>
+        )
+    })
+
     return (
         <div>
             <header className="p-5 font-bold text-center bg-chestnut-800 text-tan">
                 <h1 className="font-js text-5xl">Book Chronicle</h1>
+                <ul className="">
+                    {navData}
+                </ul>
             </header>
         </div>
     )
---
+++ client/src/constants/nav.js
@@ -0,0 +1,12 @@
+const navLinks = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'Search',
+        path: '/search'
+    }
+]
+
+export default navLinks
