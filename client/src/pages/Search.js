8fbf45cd84b2f1aa00cef518ad3f2b462420d8b8
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2024-02-01 23:41:19 -0500

    adds fallbacks for loading, errors, and no data when fetching, and adds temporary data mapping function to test api results displaying on the page. Search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,18 +3,41 @@ import { IoIosArrowDown } from "react-icons/io";
 
 function Search() {
   const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     fetch("https://openlibrary.org/search.json?author=george+r+r+martin")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data))
+      .then(() => setLoading())
+      .catch(setError);
 
-    console.log(data.docs);
+    // console.log(data.docs);
   });
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <p>{JSON.stringify(error, null, 2)}</p>;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  let docs = data.docs;
 
   return (
     <div>
       <main className="bg-tan font-ws pt-4 flex flex-col items-center">
+        <ul>
+          {docs.map((item, i) => {
+            return <li key={i}>{item.title}</li>;
+          })}
+        </ul>
         <form className="max-w-2xl mx-2 shadow-xl m-8 bg-white border flex border-charcoal rounded p-2">
           <div className="flex py-2">
             <input
